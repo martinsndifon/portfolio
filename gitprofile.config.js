7886005ea809a99fe8514db1340da357000923b8
martinsndifon <martinsndifon@gmail.com>
2023-10-14 16:47:54 +0100

    Added google analytic id

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -117,7 +117,7 @@ const config = {
     limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-5731ZPJ2CN', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
