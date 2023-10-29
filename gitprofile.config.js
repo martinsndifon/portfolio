bf05c60b4df66a4363d9cbfb223f4135da750816
martinsndifon <martinsndifon@gmail.com>
2023-10-29 23:52:46 +0100

    changed default theme back to dark

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -141,7 +141,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'light',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
