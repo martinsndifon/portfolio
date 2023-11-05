9d198322d118683e88b60973d0de23ba18b946fa
martinsndifon <martinsndifon@gmail.com>
2023-11-05 14:41:23 +0100

    More seo optimization

--- src/components/head-tag-editor/index.jsx
+++ src/components/head-tag-editor/index.jsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import PropTypes from 'prop-types';
 import { isDarkishTheme } from '../../helpers/utils';
 
-const HeadTagEditor = ({ profile, theme, googleAnalytics, social }) => {
+const HeadTagEditor = ({ profile, theme, googleAnalytics }) => {
   return (
     <Fragment>
       {profile && (
@@ -22,13 +22,10 @@ const HeadTagEditor = ({ profile, theme, googleAnalytics, social }) => {
                 gtag('config', '${googleAnalytics.id}');`}
             </script>
           )}
-          <title>Portfolio{profile.name && ` of ${profile.name}`}</title>
           <meta
             name="theme-color"
             content={isDarkishTheme(theme) ? '#000000' : '#ffffff'}
           />
-
-          <meta name="description" content={profile.bio} />
 
           <meta
             itemProp="name"
@@ -36,23 +33,6 @@ const HeadTagEditor = ({ profile, theme, googleAnalytics, social }) => {
           />
           <meta itemProp="description" content={profile.bio} />
           <meta itemProp="image" content={profile.avatar} />
-
-          <meta property="og:url" content={social?.website || ''} />
-          <meta property="og:type" content="website" />
-          <meta
-            property="og:title"
-            content={`Portfolio${profile.name && ` of ${profile.name}`}`}
-          />
-          <meta property="og:description" content={profile.bio} />
-          <meta property="og:image" content={profile.avatar} />
-
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            name="twitter:title"
-            content={`Portfolio${profile.name && ` of ${profile.name}`}`}
-          />
-          <meta name="twitter:description" content={profile.bio} />
-          <meta name="twitter:image" content={profile.avatar} />
         </Helmet>
       )}
     </Fragment>
